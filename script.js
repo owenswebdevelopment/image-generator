95aeb5846fa7908d5a02defec671d1080b730f64
Owen <139211753+owenswebdevelopment@users.noreply.github.com>
2023-09-29 00:05:40 +0900

    Add files via upload

--- script.js
+++ script.js
@@ -10,7 +10,7 @@ let page = 1;
 
 async function searchImages() {
   inputData = searchInput.value;
-  const url = `https://unsplash.com/search/photos?page=${page}&query=${inputData}&client_id=${accessKey}`;
+  const url = `https://api.unsplash.com/search/photos?page=${page}&query=${inputData}&client_id=${accessKey}`;
 
   const response = await fetch(url);
   const data = await response.json();
